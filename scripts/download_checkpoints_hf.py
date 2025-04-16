851dfff2b28673a12a157d6593468529b6d59cdf
David Heineman <david@davidheineman.com>
2025-04-16 13:55:06 -0700

    add more models

--- scripts/download_checkpoints_hf.py
+++ scripts/download_checkpoints_hf.py
@@ -9,17 +9,27 @@ WEKA_CHECKPOINTS = [f'/oe-training-default/ai2-llm/checkpoints/OLMo-medium/petei
 S3_STEPS = [0, 1000, 10000, 100000, 1100, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 19850, 2000, 20000, 21000, 22000, 23000, 24000, 25000, 26000, 27000, 28000, 29000, 3000, 30000, 31000, 32000, 33000, 34000, 35000, 36000, 37000, 38000, 39000, 4000, 40000, 41000, 42000, 43000, 44000, 45000, 46000, 47000, 48000, 49000, 5000, 50000, 51000, 52000, 53000, 54000, 55000, 56000, 57000, 58000, 59000, 6000, 60000, 61000, 62000, 63000, 64000, 65000, 66000, 67000, 68000, 69000, 7000, 70000, 71000, 72000, 73000, 74000, 75000, 76000, 77000, 78000, 79000, 8000, 80000, 81000, 82000, 83000, 84000, 85000, 86000, 87000, 88000, 89000, 9000, 90000, 91000, 92000, 93000, 93400, 94000, 95000, 96000, 97000, 98000, 99000]
 S3_CHECKPOINTS = [f's3://ai2-llm/checkpoints/OLMo-medium/peteish13-highlr/step{step}' for step in S3_STEPS]
 
+# REPO_ID = "allenai/OLMo-2-1124-7B"
+# SAVE_DIR = "OLMo-medium/peteish7"
+
+# REPO_ID = "allenai/OLMo-2-1124-13B"
+# SAVE_DIR = "OLMo-medium/peteish13-highlr"
+
+REPO_ID = "allenai/OLMo-2-0325-32B"
+SAVE_DIR = "OLMo-large/peteish32"
+
 def get_olmo_branches(num_checkpoints):
     api = HfApi()
     
     # Get all branches for the repo
-    branches = api.list_repo_refs("allenai/OLMo-2-1124-13B")
+    branches = api.list_repo_refs(REPO_ID)
     branches = [branch.name for branch in branches.branches]
     
     # Filter and sort branches that match the pattern
     steps = []
     stage1_branches = []
-    for branch in branches + WEKA_CHECKPOINTS + S3_CHECKPOINTS:
+    # for branch in branches + WEKA_CHECKPOINTS + S3_CHECKPOINTS:
+    for branch in branches:
         branch = str(branch)
         if branch.startswith('stage1-step'):
             step = int(branch.split('step')[1].split('-')[0])
@@ -49,21 +59,24 @@ def get_olmo_branches(num_checkpoints):
 
     print(f'Seeing {len(stage1_branches)} branches.')
     
-    # Get evenly spaced indices
-    total_checkpoints = len(steps)
-    if total_checkpoints <= num_checkpoints:
-        return stage1_branches.tolist()
+    # # Get evenly spaced indices
+    # total_checkpoints = len(steps)
+    # if total_checkpoints <= num_checkpoints:
+    #     return stage1_branches.tolist()
         
-    ideal_steps = np.linspace(steps[0], steps[-1], num_checkpoints)
+    # ideal_steps = np.linspace(steps[0], steps[-1], num_checkpoints)
     
-    selected_indices = []
-    for target in ideal_steps:
-        idx = np.abs(steps - target).argmin()
-        if idx not in selected_indices:  # Avoid duplicates
-            selected_indices.append(idx)
+    # selected_indices = []
+    # for target in ideal_steps:
+    #     idx = np.abs(steps - target).argmin()
+    #     if idx not in selected_indices:  # Avoid duplicates
+    #         selected_indices.append(idx)
     
-    # Get final evenly-spaced branches
-    selected_branches = stage1_branches[selected_indices].tolist()
+    # # Get final evenly-spaced branches
+    # selected_branches = stage1_branches[selected_indices].tolist()
+
+    selected_branches = stage1_branches[-31:].tolist()
+
     return selected_branches
 
 if __name__ == '__main__':
@@ -77,7 +90,7 @@ if __name__ == '__main__':
     branches = get_olmo_branches(num_checkpoints=150)
 
     # Create destination directory if it doesn't exist
-    dest_dir = '/oe-eval-default/ai2-llm/checkpoints/OLMo-medium/peteish13-highlr'
+    dest_dir = f'/oe-eval-default/ai2-llm/checkpoints/{SAVE_DIR}'
     os.makedirs(dest_dir, exist_ok=True)
 
     # Process each branch and copy/sync to destination
@@ -101,5 +114,5 @@ if __name__ == '__main__':
             # Hugging Face download
             if not os.path.exists(dest_path):
                 print(f"Downloading {branch} to {dest_path}")
-                repo_id = "allenai/OLMo-2-1124-13B"
+                repo_id = REPO_ID
                 os.system(f"HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download {repo_id} --local-dir {dest_path} --revision {branch}")
--- scripts/launch_eval.py
+++ scripts/launch_eval.py
@@ -6,7 +6,7 @@ sys.path.append(str(parent_dir))
 
 from analysis.utils import weka_to_gcs
 
-from analysis.utils.constants_models import MODEL_LADDER_LIST, MODEL_LIST_MIXES_FINAL, MODEL_LIST_MIXES_FINAL_EXTENDED, MODEL_LIST_INTERMEDIATE, MODEL_LIST_INTERMEDIATE_13B, MODEL_LIST_MIXES, OE_EVAL_BASE_MODELS, OE_EVAL_INSTRUCT_MODELS, OE_EVAL_ALL_MODELS, OE_EVAL_BASE_MODELS_EXTENDED
+from analysis.utils.constants_models import MODEL_LADDER_LIST, MODEL_LIST_MIXES_FINAL, MODEL_LIST_MIXES_FINAL_EXTENDED, MODEL_LIST_INTERMEDIATE, MODEL_LIST_INTERMEDIATE_13B, MODEL_LIST_MIXES, OE_EVAL_BASE_MODELS, OE_EVAL_INSTRUCT_MODELS, OE_EVAL_ALL_MODELS, OE_EVAL_BASE_MODELS_EXTENDED, OE_EVAL_BASE_MODELS_EXTENDED_2, MODEL_LIST_INTERMEDIATE_7B, MODEL_LIST_INTERMEDIATE_32B
 from analysis.utils.constants_final_6_ckpts import MODEL_LIST_FINAL_SIX_CKPTS
 from analysis.utils.constants_models import WEKA_CLUSTERS, GCP_CLUSTERS
 from analysis.utils.constants_tasks import MC_TASKS_COPY_COLORS, MISSING_EVALS
@@ -28,13 +28,16 @@ from analysis.utils.constants_tasks import PERTURB_COT_TASKS
 from analysis.utils.constants_tasks import PALOMA, LLM_COMPRESSION, CUSTOM_LOSS
 
 MODEL_LIST_ALL = []
-MODEL_LIST_ALL += MODEL_LADDER_LIST
-MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE
-MODEL_LIST_ALL += OE_EVAL_BASE_MODELS
-MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE_13B # 13B intermediate ckpts
-MODEL_LIST_ALL += MODEL_LIST_MIXES_FINAL # ian's new mixes
-MODEL_LIST_ALL += MODEL_LIST_MIXES_FINAL_EXTENDED # extended set of data mixes
-MODEL_LIST_ALL += OE_EVAL_BASE_MODELS_EXTENDED # OLL 2 leaderboard models
+# MODEL_LIST_ALL += MODEL_LADDER_LIST
+# MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE
+# MODEL_LIST_ALL += OE_EVAL_BASE_MODELS
+# MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE_13B # 13B intermediate ckpts
+# MODEL_LIST_ALL += MODEL_LIST_MIXES_FINAL # ian's new mixes
+# MODEL_LIST_ALL += MODEL_LIST_MIXES_FINAL_EXTENDED # extended set of data mixes
+# MODEL_LIST_ALL += OE_EVAL_BASE_MODELS_EXTENDED # OLL 2 leaderboard models
+# MODEL_LIST_ALL += OE_EVAL_BASE_MODELS_EXTENDED_2 # Additional external models
+MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE_7B # 7B Final 30 ckpts (1000 steps apart)
+MODEL_LIST_ALL += MODEL_LIST_INTERMEDIATE_32B # 32B Final 30 ckpts (1000 steps apart)
 
 # MODEL_LIST_ALL += MODEL_LIST_MIXES # not used for now
 # MODEL_LIST_ALL += OE_EVAL_INSTRUCT_MODELS # not used for now
@@ -95,52 +98,18 @@ TASK_LIST_ALL += [
     'aime::none',
 ]
 
-TASK_LIST_ALL += PALOMA
-TASK_LIST_ALL += LLM_COMPRESSION
-TASK_LIST_ALL += CUSTOM_LOSS
+# TASK_LIST_ALL += PALOMA
+# TASK_LIST_ALL += LLM_COMPRESSION
+# TASK_LIST_ALL += CUSTOM_LOSS
 
 # # FOR TESTING
 # TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'mmlu_' not in task] # exclude MMLU (long arg lists may crash beaker! https://github.com/allenai/beaker/issues/5530)
-# TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'mmlu_' in task] # <- MMLU only
-# TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'mmlu_' in task and ':para' in task] # <- MMLU:para only
-# TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'coqa:' not in task] # <- coqa is not setup properly
-# TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'hellaswag' in task] # <- hs is not setup properly
-# TASK_LIST_ALL = [task for task in TASK_LIST_ALL if 'gsm8k' in task] # <- gsm is not setup properly
-# MODEL_LIST_ALL = [MODEL_LIST_INTERMEDIATE[1]] # <- only use first model!
-# MODEL_LIST_ALL = [OE_EVAL_INSTRUCT_MODELS[0]]
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if 'peteish7' in model] # <- 3B models!
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if '-3B-5xC' in model or 'peteish13' in model] # <- only peteish13!
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if '-3B-' in model] # <- 3B models!
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if '-1B-10xC' in model or '-3B-10xC' in model] # <- only 1B-10xC !
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if 'falcon_and_cc-1B-5xC' in model or '-3B-10xC' in model] # <- only 1B-10xC !
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if 'qwen2.5-72b' in model or 'qwen2.5-32b' in model or 'qwen2.5-14b' in model] # <- only 3B ladder models!
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if 'qwen2.5-7b' in model]
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if model in ['llama3.1-8b', 'pythia-1b', 'olmoe-1b-7b-0924', 'llama3.2-1b', 'qwen2.5-72b', 'llama2-7b', 'llama3-8b']]
-# MODEL_LIST_ALL = [model for model in MODEL_LIST_ALL if model in ['gemma-2b', 'llama2-13b', 'phi-1.5', 'olmo-7b-0724', 'olmo-1b', "llama3.1-70b"]]
-# MODEL_LIST_ALL = ["llama3.1-70b", "qwen2.5-3b", "qwen2.5-7b", "qwen2.5-72b"] # autobencher mc
-# MODEL_LIST_ALL = ["qwen2.5-32b", "qwen2.5-14b", "qwen2.5-72b"] # llm compression
-# MODEL_LIST_ALL = OE_EVAL_BASE_MODELS
-# TASK_LIST_ALL = SYNTHETIC_TASKS # <- only use synthetic tasks!
-# TASK_LIST_ALL = AGI_EVAL_MC + BBH_COT
-# TASK_LIST_ALL = ['coqa::olmes:full', 'copycolors_4way:mc::none']
-# TASK_LIST_ALL = ['gsm_plus::none']
-# TASK_LIST_ALL = ['autobencher:mc::none']
-# TASK_LIST_ALL = LLM_COMPRESSION
-# TASK_LIST_ALL = ['sky_t1::custom_loss', 'numia_math::custom_loss', 'tulu_if::custom_loss']
-# TASK_LIST_ALL = PALOMA
-# TASK_LIST_ALL = ['arc_challenge:rc::olmes:full']
-# MODEL_LIST_ALL = [
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/no_reddit-4M-5xC/step5735-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/no_flan-4M-5xC/step5735-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dolma17-75p-DCLM-baseline-25p-4M-5xC/step5735-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dolma17-50p-DCLM-baseline-50p-4M-5xC/step5735-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dolma17-25p-DCLM-baseline-75p-90M-5xC/step29901-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dolma17-25p-DCLM-baseline-75p-60M-5xC/step29052-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dolma17-25p-DCLM-baseline-75p-4M-5xC/step5735-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/dclm_ft7percentile_fw2-60M-5xC/step29042-unsharded-hf",
-#     "weka://oe-eval-default/ai2-llm/checkpoints/OLMo-ladder/davidh/DCLM-baseline-20M-5xC/step14594-unsharded-hf",
-# ]
-# MODEL_LIST_ALL = ['weka://oe-training-default/ai2-llm/checkpoints/OLMo-ladder/peteish-moreeval-rerun-1B-1xC/step16279-unsharded-hf'] # model used for question text
+MODEL_LIST_ALL = MODEL_LIST_ALL
+TASK_LIST_ALL = TASK_LIST_ALL
+
+
+# MODEL_LIST_ALL = ["pythia-14m"]
+TASK_LIST_ALL = ["mmlu_high_school_european_history:rc::olmes:full"]
 
 
 def run_eval(model_list, task_list, model_type='hf', gpus=1, limit=None, batch_size=None, save_requests=True):
@@ -149,12 +118,23 @@ def run_eval(model_list, task_list, model_type='hf', gpus=1, limit=None, batch_s
     if not isinstance(model_list, list): 
         model_list = [model_list]
 
+    # # Use WEKA
+    # cluster_list = WEKA_CLUSTERS
+    # model_list = [model.replace('weka://', '/weka-mount/') for model in model_list]
+
+    # Use GCP
     cluster_list = GCP_CLUSTERS
     if any('weka://' in model for model in model_list):
         model_list = [weka_to_gcs(model) for model in model_list]
 
     if len(model_list) == 1: # convert back list -> str
         model_list = model_list[0]
+
+    WORKSPACE = "ai2/ladder-evals"
+    PRIORITY = "normal"
+
+    # WORKSPACE = "ai2/lm-eval"
+    # PRIORITY = "high"
 
     command = f"""
     oe-eval \
@@ -163,31 +143,20 @@ def run_eval(model_list, task_list, model_type='hf', gpus=1, limit=None, batch_s
         --cluster {cluster_list} \
         --model-type {model_type} \
         --gpus {gpus} \
-        --beaker-workspace ai2/ladder-evals \
+        --beaker-workspace {WORKSPACE} \
         --beaker-image davidh/oe-eval-metaeval \
         --gantry-secret-aws-access-key-id AWS_ACCESS_KEY_ID \
         --gantry-secret-aws-secret-access AWS_SECRET_ACCESS_KEY \
+        --gantry-secret-hf-read-only HF_TOKEN \
         --remote-output-dir s3://ai2-llm/eval-results/downstream/metaeval/ \
         --recompute-metrics \
-        --beaker-priority normal
+        --gantry-args '{{"env": "VLLM_USE_V1=0"}}' \
+        --beaker-priority {PRIORITY}
     """
-
-    # command = f"""
-    # oe-eval \
-    #     --model {model_list} \
-    #     --task {task_list} \
-    #     --cluster {cluster_list} \
-    #     --model-type {model_type} \
-    #     --gpus {gpus} \
-    #     --beaker-workspace ai2/lm-eval \
-    #     --beaker-image davidh/oe-eval-metaeval \
-    #     --gantry-secret-aws-access-key-id lucas_AWS_ACCESS_KEY_ID \
-    #     --gantry-secret-aws-secret-access lucas_AWS_SECRET_ACCESS_KEY \
-    #     --remote-output-dir s3://ai2-llm/eval-results/downstream/metaeval/ \
-    #     --recompute-metrics \
-    #     --beaker-priority high
-    # """
+    # --gantry-secret-hf-read-only lucas_HUGGING_FACE_HUB_TOKEN \
     # --run-local \
+
+    # oe-eval --model pythia-14m --task mmlu_high_school_european_history:rc::olmes:full --gpus 1 --model-type vllm --run-local
 
     command = command.replace('\n', '').replace('  ', '') # remove extra spacing!
     if limit is not None: 
@@ -232,7 +201,7 @@ def main():
             # From my testing, looks like anything less than 4 GPUs on 13B+ models (or Gemma 7B+) breaks
             # Also 70B model do not work on neptune (L40s)
             model_type = 'vllm'
-            if model in ['gemma-7b', 'gemma2-9b', "gemma2-2b-instruct", "gemma2-9b-instruct", "gemma2-9b-instruct-SimPO", "llama2-13b", "llama3-70b", "llama3.1-70b", "qwen2.5-14b", "qwen2.5-32b", "qwen2.5-72b", "llama3.1-70b-instruct", "qwen2.5-14b-instruct"] or '32B' in model or '72B' in model or '22B' in model or '15b' in model or '40b' in model or '110B' in model or '70B' in model:
+            if model in ['gemma-7b', 'gemma2-9b', "gemma2-2b-instruct", "gemma2-9b-instruct", "gemma2-9b-instruct-SimPO", "llama2-13b", "llama3-70b", "llama3.1-70b", "qwen2.5-14b", "qwen2.5-32b", "qwen2.5-72b", "llama3.1-70b-instruct", "qwen2.5-14b-instruct"] or '32B' in model or '72B' in model or '22B' in model or '15b' in model or '40b' in model or '110B' in model or '70B' in model or model in OE_EVAL_BASE_MODELS_EXTENDED_2:
                 gpus = 4
             else:
                 gpus = 1 # don't need many GPUs for small models
@@ -266,18 +235,3 @@ def main():
 
 
 if __name__ == '__main__': main()
-
-
-# Models that broke on 1 GPU HF
-# MODEL_LIST_ALL = [
-#     "dclm-1b", "dclm-7b", "gemma-7b", "gemma2-9b", "llama2-7b",
-#     "llama3-70b", "llama3.1-70b", "olmo-7b-0424", "qwen2.5-32b", "qwen2.5-72b",
-#     "llama-7b",
-#     "weka://oe-training-default/ai2-llm/checkpoints/OLMo-medium/peteish13-highlr/step476848-hf-vllm",
-# ]
-
-# Scaling data constrained LLMs (https://arxiv.org/abs/2305.16264)
-# https://huggingface.co/datablations#models / https://github.com/huggingface/datablations
-
-# (probably your best bet) 20M to 3.3B:
-# https://huggingface.co/KempnerInstituteAI/loss-to-loss
